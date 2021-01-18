c47c2a9edbf32768832f76034c29aad0ea9987f7
Jeongho Nam <samchon@samchon.org>
2021-01-18 17:36:37 +0900

    Add Has on "index.ts"

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./Belongs";
 export * from "./EncryptedColumn";
+export * from "./Has";
 export * from "./IncrementalColumn";
 export * from "./NamingStrategy";
 export * from "./Password";
