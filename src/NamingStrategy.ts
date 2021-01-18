93d1d9cc7be84a2630644cbe5cffa28aaf075503
Jeongho Nam <samchon@samchon.org>
2021-01-18 13:18:00 +0900

    Add PasswordClosure

--- src/NamingStrategy.ts
+++ src/NamingStrategy.ts
@@ -1,7 +1,7 @@
-import * as orm from  "typeorm";
 import pluralize from "pluralize";
+import { DefaultNamingStrategy } from "typeorm";
 
-export class NamingStrategy extends orm.DefaultNamingStrategy
+export class NamingStrategy extends DefaultNamingStrategy
 {
     public tableName(targetName: string, userSpecifiedName: string | undefined): string
     {
@@ -9,9 +9,40 @@ export class NamingStrategy extends orm.DefaultNamingStrategy
             userSpecifiedName = pluralize(pascal_to_snake(targetName));
         return userSpecifiedName;
     }
+
+    public closureJunctionTableName(originalClosureTableName: string): string
+    {
+        return pluralize(pascal_to_snake(originalClosureTableName)) + "_closure";
+    }
+
+    public columnName(propertyName: string, customName: string, embeddedPrefixes: string[]): string
+    {
+        if (!customName)
+            customName = propertyName;
+        return [...embeddedPrefixes, customName].map(str => pascal_to_snake(str)).join("_");
+    }
+
+    public relationName(propertyName: string): string
+    {
+        return pascal_to_snake(propertyName);
+    }
 }
 
 function pascal_to_snake(name: string): string
 {
     return name.split(/(?=[A-Z])/).map(str => str.toLowerCase()).join("_");
 }
+
+// function get_composite_name(tableOrName: Table | string, columnNames: string[], where?: string): string
+// {
+//     if (tableOrName instanceof Table)
+//     {
+//         const index: number = tableOrName.name.indexOf(".");
+//         tableOrName = tableOrName.name.substr(index + 1);
+//     }
+
+//     const tuple: string[] = [pluralize(tableOrName), ...columnNames];
+//     if (where)
+//         tuple.push(where);
+//     return tuple.map(str => pascal_to_snake(str)).join("_");
+// }
---
+++ src/PasswordClosure.ts
@@ -0,0 +1,48 @@
+import * as bcrypt from "bcryptjs";
+import * as orm from "typeorm";
+
+/**
+ * Closure table for the password.
+ * 
+ * @author Jeongho Nam - https://github.com/samchon
+ */
+@orm.Tree("closure-table")
+export class PasswordClosure
+{
+    /**
+     * The password.
+     */
+    @orm.Column("varchar", { length: 1000 })
+    private password!: string;
+
+    /**
+     * Configure password.
+     * 
+     * Configure the password with one-way encryption. Therefore, no one can know the 
+     * original password, even if the one is a system administrator.
+     * 
+     * @param str The password to configure.
+     */
+    public async set(str: string): Promise<void>
+    {
+        const salt: string = await bcrypt.genSalt();
+        this.password = await bcrypt.hash(str, salt);
+    }
+
+    /**
+     * Test whether the password is matched with the original.
+     * 
+     * The password configured in this record has been one-way encrypted by `bcrypt`
+     * algorithm. Therefore, no one can know the original password. Therefore, just 
+     * only testing the equality through this {@link equals}() is possible.
+     * 
+     * @param str Target to be compared.
+     * @return Whether equal or not.
+     */
+    public async equals(str: string): Promise<boolean>
+    {
+        return (this.password === null)
+            ? false
+            : bcrypt.compare(str, this.password);
+    }
+}
